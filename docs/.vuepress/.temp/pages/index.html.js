ffdf749064168ff0fdfeb4f681279b96f17b0eab
bu.xu_ex <bu.xu@hand-china.com>
2021-08-17 10:40:52 +0800

    1

---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,20 @@
+export const data = {
+  "key": "v-8daa1a0e",
+  "path": "/",
+  "title": "Hello VuePress",
+  "lang": "zh-CN",
+  "frontmatter": {},
+  "excerpt": "",
+  "headers": [],
+  "filePathRelative": "README.md",
+  "git": {
+    "updatedTime": 1628829305000,
+    "contributors": [
+      {
+        "name": "bu.xu_ex",
+        "email": "bu.xu@hand-china.com",
+        "commits": 1
+      }
+    ]
+  }
+}
